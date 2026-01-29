9d04fda837b2a6fcea74f11887e9ae428fc02331
frosb1official-lang <frosb1official@gmail.com>
2026-01-29 16:40:24 +0700

    feat: add new tabs

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+const modalButton = document.querySelectorAll('.more')
+const modal = document.querySelector('.modal')
+const overlay = modal.querySelector('.overlay')
+const closeButton = modal.querySelector('.modal__close')
+
+
+modalButton.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        modal.classList.toggle('hidden')
+    })
+    overlay.addEventListener('click', () => {
+    modal.classList.add('hidden')
+    })
+
+    closeButton.addEventListener('click', () => {
+        modal.classList.add('hidden')
+    })
+})
